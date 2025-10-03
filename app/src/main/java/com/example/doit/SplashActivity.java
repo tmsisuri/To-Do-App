04c9de8909e0ee9d4582c943944854f89e1a3c45
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2025-10-03 18:04:08 +0530

    add splash activity files

---
+++ app/src/main/java/com/example/doit/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.doit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },1000);
+    }
+}
